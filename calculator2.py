aca250d130122ab92a5d02fd2bc456250b49a100
Manasi17Patel <143248672+Manasi17Patel@users.noreply.github.com>
2023-08-27 13:48:52 +0530

    Add files via upload

---
+++ calculator2.py
@@ -0,0 +1,14 @@
+first = int(input("enter first number:"))
+second = int(input("enter second number:"))
+
+a = first + second
+print("sum of two number:",a)
+
+b = first - second
+print("subscription of two number:",b)
+
+c = first * second
+print("multiplication of two number:",c)
+
+d = first / second
+print("division of two number:",d)
